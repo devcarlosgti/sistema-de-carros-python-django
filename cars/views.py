77412d6d12769db0e12b3409b62554f2b4e6ae8f
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-08-28 15:30:43 -0300

    Entendendo templates

--- cars/views.py
+++ cars/views.py
@@ -4,4 +4,17 @@ from django.http import HttpResponse
 # Create your views here.
 
 def cars_view(request):
-    return HttpResponse('Meus carros testes')
+    #html temporário
+    html = '''
+    <html>
+        <head>
+            <title>Meus carros</title>
+        </head>
+        <body>
+            <h1>Carros do Carlos</h1>
+            <h3>Só carros top!</h3>
+        </body>
+    </html>
+    '''
+    return HttpResponse(html)
+    # return HttpResponse('<h1>Meus carros testes</h1>')
