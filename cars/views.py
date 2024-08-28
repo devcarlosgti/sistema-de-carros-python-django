99da041b934f007b61dba618a4a20c44b7c3a630
Carlos-GleysonTI <c.gleysonti@gmail.com>
2024-08-28 15:58:13 -0300

    Retornando Templates para o usuário

--- cars/views.py
+++ cars/views.py
@@ -1,20 +1,9 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 # Create your views here.
+# def cars_view(request):
+#     return HttpResponse()
 
 def cars_view(request):
-    #html temporário
-    html = '''
-    <html>
-        <head>
-            <title>Meus carros</title>
-        </head>
-        <body>
-            <h1>Carros do Carlos</h1>
-            <h3>Só carros top!</h3>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
-    # return HttpResponse('<h1>Meus carros testes</h1>')
+    return render(request, 'cars.html') #cars.html esta na pasta de templates
